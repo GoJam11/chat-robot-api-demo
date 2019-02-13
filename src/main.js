3f1bf963cb3ceb222aee769a5ee9b06e3f829f7a
guo <iloveos@live.com>
2019-02-13 14:40:35 +0800

    3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueResource from 'vue-resource'
-Vue.use(VueAxios, axios, VueResource)
 import VueJsonp from 'vue-jsonp'
 Vue.use(VueJsonp)
 
